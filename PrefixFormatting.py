5b2ab1ce8e5d3acff693f74e4c4412baeda143d0
Henry F <henry.foley@mymail.champlain.edu>
2023-09-02 17:20:48 -0400

    Initial UI work
    UI Displays checkboxes and text boxes that are initialized by the JSON file

--- PrefixFormatting.py
+++ PrefixFormatting.py
@@ -17,7 +17,49 @@ prefixFormattingOptionsJson = []
 def PrefixFormattingMain():
     filePath = os.path.join(os.path.abspath(os.path.dirname(__file__)), "prefixFormattingSettings.json")
     loadJsonFile(filePath)
+    PrefixFormattingUI()
+    setObjectPrefix()
     assignPrefix()
+
+
+def PrefixFormattingUI():
+    # Delete Window if it already exists
+    if cmds.window('PrefixFormattingWin', exists=True):
+        print('Window exists! Deleting window!')
+        cmds.deleteUI('PrefixFormattingWin', window=True)
+
+    cmds.window('PrefixFormattingWin', t="Prefix Formatting", mnb=False, mxb=False, s=False)
+
+    cmds.columnLayout('mainUI_A', parent='PrefixFormattingWin')
+    cmds.rowColumnLayout(nc=1, cw=[(1, 200)], p='mainUI_A')
+
+    cmds.separator(h=10, style='none')
+    cmds.iconTextStaticLabel(st='textOnly', l='Prefix Format Options', align='center', font='boldLabelFont')
+    cmds.separator(h=10, style='none')
+
+    cmds.rowColumnLayout('CheckBoxColumn', nc=2, cw=[(1, 100), (2, 100)], cs=[(1, 10), (2, 10)], p='mainUI_A')
+
+    # Check Boxes
+    cmds.frameLayout(labelVisible=False, width=75, p='CheckBoxColumn')
+    cmds.iconTextStaticLabel(st='textOnly', l="Object Type")
+    cmds.checkBox('Asset_CB', l="Asset", height=20, value=False, cc = lambda args: printSomething())
+    cmds.separator(height=10, style='in')
+    cmds.checkBox('Geometry_CB', l="Geometry", height=20, value=True)
+    cmds.separator(height=10, style='in')
+    cmds.checkBox('NURBS_Objects_CB', l="NURBS Objects", height=20, value=False)
+    cmds.separator(height=10, style='in')
+
+    # Text Fields
+    cmds.frameLayout(labelVisible=False, width=75, p='CheckBoxColumn')
+    cmds.iconTextStaticLabel(st='textOnly', l='Prefix')
+    cmds.textField('Asset_TF', height=20)
+    cmds.separator(height=10, style='in')
+    cmds.textField('Geometry_TF', height=20)
+    cmds.separator(height=10, style='in')
+    cmds.textField('NURBS_Objects_TF', height=20)
+    cmds.separator(height=10, style='in')
+
+    cmds.showWindow('PrefixFormattingWin')
 
 
 def loadJsonFile(filePath):
@@ -38,7 +80,23 @@ def loadJsonFile(filePath):
         else:
             return None
 
+
+def setObjectPrefix():
+    cmds.textField('Asset_TF', w=5, e=True, tx=prefixFormattingOptionsJson['Asset'])
+    cmds.textField('Geometry_TF', e=True, tx=prefixFormattingOptionsJson['Geometry'])
+    cmds.textField('NURBS_Objects_TF', e=True, tx=prefixFormattingOptionsJson['NURBS_Objects'])
+    """cmds.textField('Polygon Objects_TF', e=True, tx=prefixFormattingOptionsJson['Polygon_Objects'])
+    cmds.textField('Subdiv Objects_TF', e=True, tx=prefixFormattingOptionsJson['Subdiv_Objects'])
+    cmds.textField('GPU Cache_TF', e=True, tx=prefixFormattingOptionsJson['GPU_Cache'])
+    cmds.textField('Cameras_TF', e=True, tx=prefixFormattingOptionsJson['Cameras'])
+    cmds.textField('Joints_TF', e=True, tx=prefixFormattingOptionsJson['Joints'])"""
+
+
 def assignPrefix():
     objects = cmds.ls()
     objects.sort(key=len, reverse=True)
     print(objects)
+
+
+def printSomething():
+    print("Something")
