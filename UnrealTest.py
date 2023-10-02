7f5fe481c3c3feca3bd314fd5dc62e6093b64af2
Henry Foley <henry.foley@mymail.champlain.edu>
2023-10-02 14:44:30 -0400

    Print Out Materials

--- UnrealTest.py
+++ UnrealTest.py
@@ -1,10 +1,27 @@
 # Unreal Test - Maya
 # Henry Foley, 2023
+import time
 
-import unreal as ue
+import unreal
 
-v1 = ue.Vector()
-v1.x = 10
-v2 = ue.Vector(10, 20, 30)
-v3 = (v1 + v2) * 2
-print(v3)
+path = '/Game/StarterContent/Materials'
+
+asset_list = unreal.AssetRegistryHelpers.get_asset_registry().get_assets_by_path(path)
+#asset_list = unreal.AssetRegistryHelpers.get_asset_registry().get_assets_by_class()
+
+"""print(asset_list)
+
+for asset in asset_list:
+    print(asset)"""
+
+total_frames = len(asset_list)
+text_label = "Listing!"
+with unreal.ScopedSlowTask(total_frames, text_label) as slow_task:
+    slow_task.make_dialog(True)
+    for asset in asset_list:
+        if slow_task.should_cancel():
+            break
+        #if asset.get_class() == 'Materials':
+        print(asset)
+        time.sleep(0.1)
+        slow_task.enter_progress_frame(1)
