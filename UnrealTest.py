66c66de503d8da34a539d48532ebf6f53f2e6d64
Henry Foley <henry.foley@mymail.champlain.edu>
2023-10-24 21:30:56 -0400

    Assigning all prefixes
    Just need to fix level issue and selection of folders

--- UnrealTest.py
+++ UnrealTest.py
@@ -6,29 +6,17 @@ import unreal
 import json
 import os
 
-@unreal.uclass()
-class GlobalEditorUtilityBase(unreal.GlobalEditorUtilityBase): pass
-
-utilBase = GlobalEditorUtilityBase()
 unrealPrefixes = []
 
 path = '/Game/TestFolder'
-#path = '/Game/StarterContent/Materials'
 
 asset_list = unreal.AssetRegistryHelpers.get_asset_registry().get_assets_by_path(path)
-
-#asset_list = unreal.AssetRegistryHelpers.get_asset_registry().get_assets_by_class()
-
-"""print(asset_list)
-
-for asset in asset_list:
-    print(asset)"""
 
 
 def main():
     filePath = os.path.join(os.path.abspath(os.path.dirname(__file__)), "unrealPrefixes.json")
     loadJsonFile(filePath)
-
+    assignAllPrefixes()
 
 def loadJsonFile(filePath):
     global unrealPrefixes
@@ -52,58 +40,170 @@ def assignPrefix(asset, prefix):
     assetName = asset.get_editor_property('asset_name')
     assetTools = unreal.AssetToolsHelpers.get_asset_tools()
 
-    print("Asset:" + str(asset.get_asset()))
-    print("Package Name: " + str(path))
-    print("Renamed Asset: " + str(assetName))
-
-
     #Check if Asset has a prefix
     if "_" in str(assetName)[0:5]:
         print("Prefix already in Asset Name")
         return
     else:
         assetName = prefix + str(assetName)
+        print("Asset:" + str(asset.get_asset()))
+        print("JSON Code: " + prefix)
+        print("Renamed Asset: " + str(assetName))
         assetTools.rename_assets([unreal.AssetRenameData(asset.get_asset(), path, assetName)])
 
 
+def assignAllPrefixes():
+    total_frames = len(asset_list)
+    text_label = "Listing!"
+    with unreal.ScopedSlowTask(total_frames, text_label) as slow_task:
+        slow_task.make_dialog(True)
+        for asset in asset_list:
+            if slow_task.should_cancel():
+                break
+
+            # Get Asset Type and name
+            assetType = asset.get_class().get_name()
+            assetName = asset.get_editor_property('asset_name')
+
+            if assetType == "AnimBlueprint":
+                assignPrefix(asset, unrealPrefixes['Animation_Blueprint'])
+                continue
+
+            elif assetType == "AnimMontage":
+                assignPrefix(asset, unrealPrefixes['Animation_Montages'])
+                continue
+
+            elif assetType == "AnimSequence":
+                assignPrefix(asset, unrealPrefixes['Animation_Sequence'])
+                continue
+
+            elif assetType == "Blueprint":
+                assignPrefix(asset, unrealPrefixes['Blueprint'])
+                continue
+
+            elif assetType == "BlendSpace":
+                assignPrefix(asset, unrealPrefixes['Blend_Space'])
+                continue
+
+            elif assetType == "BehaviorTree":
+                assignPrefix(asset, unrealPrefixes['Behavior_Tree'])
+                continue
+
+            elif assetType == "CurveTable":
+                assignPrefix(asset, unrealPrefixes['Curve_Table'])
+                continue
+
+            elif assetType == "DataTable":
+                assignPrefix(asset, unrealPrefixes['Data_Table'])
+                continue
+
+            elif assetType == "UserDefinedEnum":
+                assignPrefix(asset, unrealPrefixes['Enum'])
+                continue
+
+            elif assetType == "UserDefinedStruct":
+                assignPrefix(asset, unrealPrefixes['Structure'])
+                continue
+
+            elif assetType == "TextureCube":
+                assignPrefix(asset, unrealPrefixes['HDRI'])
+                continue
+
+            elif assetType == "World":
+                assignPrefix(asset, unrealPrefixes['Level'])
+                continue
+
+            elif assetType == "LevelSequence":
+                assignPrefix(asset, unrealPrefixes['Level_Sequence'])
+                continue
+
+            elif assetType == 'Material':
+                assignPrefix(asset, unrealPrefixes['Material'])
+                continue
+
+            elif assetType == "MaterialFunction":
+                assignPrefix(asset, unrealPrefixes['Material_Function'])
+                continue
+
+            elif assetType == "MaterialInstanceConstant":
+                assignPrefix(asset, unrealPrefixes['Material_Instance'])
+                continue
+
+            elif assetType == "MediaPlayer":
+                assignPrefix(asset, unrealPrefixes['Media_Player'])
+                continue
+
+            elif assetType == "MediaTexture":
+                assignPrefix(asset, unrealPrefixes['Media_Texture'])
+                continue
+
+            elif assetType == "NiagaraEmitter":
+                assignPrefix(asset, unrealPrefixes['Niagara_Emitter'])
+                continue
+
+            elif assetType == "NiagaraScript":
+                assignPrefix(asset, unrealPrefixes['Niagara_Function'])
+                continue
+
+            elif assetType == "NiagaraSystem":
+                assignPrefix(asset, unrealPrefixes['Niagara_System'])
+                continue
+
+            elif assetType == 'PhysicsAsset':
+                assignPrefix(asset, unrealPrefixes['Physics_Asset'])
+                continue
+
+            elif assetType == 'PhysicalMaterial':
+                assignPrefix(asset, unrealPrefixes['Physics_Material'])
+                continue
+
+            elif assetType == 'ParticleSystem':
+                assignPrefix(asset, unrealPrefixes['Particle_System'])
+                continue
+
+            elif assetType == 'ReverbEffect':
+                assignPrefix(asset, unrealPrefixes['Reverb_Effect'])
+                continue
+
+            elif assetType == 'IKRigDefinition':
+                assignPrefix(asset, unrealPrefixes['Rig'])
+                continue
+
+            elif assetType == 'SoundWave':
+                assignPrefix(asset, unrealPrefixes['Sound_Wave'])
+                continue
+
+            elif assetType == 'SoundCue':
+                assignPrefix(asset, unrealPrefixes['Sound_Cue'])
+                continue
+
+            elif assetType == 'SoundClass':
+                assignPrefix(asset, unrealPrefixes['Sound_Class'])
+                continue
+
+            elif assetType == 'SkeletalMesh':
+                assignPrefix(asset, unrealPrefixes['Skeletal_Mesh'])
+                continue
+
+            elif assetType == 'Skeleton':
+                assignPrefix(asset, unrealPrefixes['Skeleton'])
+                continue
+
+            elif assetType == 'StaticMesh':
+                assignPrefix(asset, unrealPrefixes['Static_Mesh'])
+                continue
+
+            elif assetType == 'Texture2D':
+                assignPrefix(asset, unrealPrefixes['Texture'])
+                continue
+
+            elif assetType == 'WidgetBlueprint':
+                assignPrefix(asset, unrealPrefixes['Widget_Blueprint'])
+                continue
+
+            else:
+                print("This asset type has no prefix code: " + str(assetType))
 
 
-
+# RUN PROGRAM
 main()
-#for asset in unrealPrefixes:
- #   print(asset)
-total_frames = len(asset_list)
-text_label = "Listing!"
-with unreal.ScopedSlowTask(total_frames, text_label) as slow_task:
-    slow_task.make_dialog(True)
-    for asset in asset_list:
-        if slow_task.should_cancel():
-            break
-
-        # Get Asset Type and name
-        assetType = asset.get_class().get_name()
-        assetName = asset.get_editor_property('asset_name')
-        assetPackageName = asset.get_editor_property('package_name')
-
-        if assetType == "Blueprint":
-            print("Found Blueprint: " + str(assetName) + " JSON Code: " + unrealPrefixes['Blueprint'])
-            #print("Asset: " + str(assetPackageName))
-            assignPrefix(asset, unrealPrefixes['Blueprint'])
-            continue
-
-
-        """elif assetType == 'Material':
-            print("Found Mat" + str(assetFilePath))
-            continue
-        elif assetType == 'NiagaraSystem':
-            print("Found Niagara")
-            continue
-        elif assetType == 'StaticMesh':
-            print("Found Static Mesh: " + str(assetFilePath))
-            continue
-        else:
-            print("This asset type has no prefix code: " + str(assetFilePath))
-        # print("Asset: " + str(asset) + "Class: " + str(asset.get_class()))"""
-        time.sleep(0.1)
-        slow_task.enter_progress_frame(1)
-
